a894c2f4cb0d951071d83b53d9cf7662f14c406f
Jonah West <jonahcwest@gmail.com>
2022-11-19 17:06:06 +0000

    .

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import itertools
 from torch.utils.data import Dataset
+from PIL import Image
 import json
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+DATA_PATH = "dataset/train"
+MAX_IMAGES = 200
 
 
 class Data(Dataset):
     def __init__(self):
         parsed = 0
-        with open("dataset/train/_annotations.coco.json", "r") as f:
+        with open(f"{DATA_PATH}/_annotations.coco.json", "r") as f:
             content = f.read()
             parsed = json.loads(content)
 
@@ -23,5 +32,33 @@ class Data(Dataset):
 
             file_name_to_spots[file_name][category_id] += 1
 
+        file_name_to_spots = dict(
+            itertools.islice(file_name_to_spots.items(), MAX_IMAGES)
+        )
 
-Data()
+        self.images = []
+        for file_name in file_name_to_spots:
+            to_tensor = transforms.ToTensor()
+            normalize = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            with Image.open(f"{DATA_PATH}/{file_name}", "r") as image:
+                self.images.append(
+                    {
+                        "image": normalize(to_tensor(image)),
+                        "spots": file_name_to_spots[file_name],
+                    }
+                )
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        return self.images[index]
+
+
+def main():
+    data = Data()
+    data_iter = iter(data)
+
+
+if __name__ == "__main__":
+    main()
